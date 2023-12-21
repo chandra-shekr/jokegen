744bca223de4c915e4acbfd4c30fad076440978e
laplace <laplace@pop-os.localdomain>
2023-12-22 01:12:29 +0530

    demo joke gen api

---
+++ src/github.com/chandra-shekr/jokegen/generator/genJoke.go
@@ -0,0 +1,32 @@
+package generator
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type response struct {
+	Delivery string `json:"delivery"`
+}
+
+func GenRandomJoke() (string, error) {
+
+	resp, err := http.Get("https://v2.jokeapi.dev/joke/Any")
+
+	if err != nil {
+
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var result response
+
+	if err := json.Unmarshal(body, &result); err != nil {
+
+		return "", err
+	}
+
+	return result.Delivery, nil
+
+}
---
+++ src/github.com/chandra-shekr/jokegen/go.mod
@@ -0,0 +1,3 @@
+module jokegen
+
+go 1.21.0
